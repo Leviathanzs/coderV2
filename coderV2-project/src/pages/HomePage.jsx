271d9e71f97c7bb935054b7890ed8af8d7980400
Leviathanzs <okywidhiansyah@gmail.com>
2023-08-20 14:49:03 +0700

    coba coding sendiri

--- coderV2-project/src/pages/HomePage.jsx
+++ coderV2-project/src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
+import logo from '../assets/img/hero.png'
+
 const HomePage = () => {
     return (
         <div className="homePage">
-            <header className="w-100 min-vh-100"></header>
+            <header className="w-100 min-vh-100">
+                <section className="home" id="home">
+                    <div className="home-content">
+                        <h1>Temukan Bakat Kreatifmu Bersama Kami!</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur, molestiae.</p>
+                        <div className="btn-container">
+                            <a href="" className='btn-1'>Lihat Kelas</a>
+                            <a href="" className='btn-2'>Lihat Promo</a>
+                        </div>
+                    </div>
+                    <div className="hero-img">
+                        <img src={logo} alt="rocket" />
+                    </div>
+                </section>
+            </header>
         </div>
 
     )
