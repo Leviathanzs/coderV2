ff0c41cd8b93622859577ebcaeba876ddca48043
Leviathanzs <okywidhiansyah@gmail.com>
2023-08-22 11:15:35 +0700

    Page kelas selesai

--- coderV2-project/src/pages/KelasPage.jsx
+++ coderV2-project/src/pages/KelasPage.jsx
@@ -3,16 +3,37 @@ import {semuaKelas} from '../data/index'
 
 const KelasPage = () => {
     return (
-        <div className="kelas">
+        <section className="kelas min-vh-100">
             <Container>
                 <Row>
                     <Col>
-                        <h1>Semua Kelas</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                        <h1 className="fw-bold text-center">Semua Kelas</h1>
+                        <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </Col>
                 </Row>
+                <Row>
+                {semuaKelas.map((kelas) => {
+                    return (
+                        <Col key={kelas.id} className='shadow rounded'>
+                            <img src={kelas.image} alt="unsplash.com" className="w-100 mb-5 rounded-top" />
+                            <div className="star mb-2 px-3">
+                                <i className={kelas.star1}></i>
+                                <i className={kelas.star2}></i>
+                                <i className={kelas.star3}></i>
+                                <i className={kelas.star4}></i>
+                                <i className={kelas.star5}></i>
+                            </div>
+                            <h5 className="mb-5 px-3 fw-bold">{kelas.title}</h5>
+                            <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                                <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                                <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                            </div>
+                        </Col>
+                    )
+                })}
+                </Row>
             </Container>
-        </div>
+        </section>
     )
 }
 
